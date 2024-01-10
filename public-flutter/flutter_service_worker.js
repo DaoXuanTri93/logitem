be3bb3f239b31511927ae0cc25925b925e925b00
BRYCEN-VN\dx_tri <katoukid1993@gmail.com>
2024-01-10 17:07:23 +0700

    micro

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "a809f5a0b6dd4627e009264b3d8f06f1
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dd81f2273a18251b694964527b68a101",
-"/": "dd81f2273a18251b694964527b68a101",
-"main.dart.js": "7376228398ac5649d35e3a159398a4db",
+"index.html": "87c50f4e15f14b59621dc89b65900f65",
+"/": "87c50f4e15f14b59621dc89b65900f65",
+"main.dart.js": "996a3925963ea212e3e94205d7e70ff9",
 "manifest.json": "a3b941730fff719b4b41190d4d43f008",
 "version.json": "0253b4f5390261cb3679fc1177344210"};
 // The application shell files that are downloaded before a service worker can
